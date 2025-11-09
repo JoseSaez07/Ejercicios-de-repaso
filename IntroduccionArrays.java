e3ac1f09a01665de228ef6dcc25fe08ff2c06fa9
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-09 16:01:38 +0100

    s
    s

---
+++ IntroduccionArrays.java
@@ -0,0 +1,71 @@
+public class mostrarTemperaturas {
+    /**
+     * La clase {@code IntroduccionArrays} sirve como ejemplo básico para introducir
+     * el uso de <b>arrays unidimensionales</b> en Java.
+     * <p>
+     * En este ejemplo se muestra cómo:
+     * <ul>
+     * <li>Declarar y crear un array de tipo {@code int}.</li>
+     * <li>Asignar valores a cada elemento del array.</li>
+     * <li>Pasar un array como argumento a un método.</li>
+     * <li>Recorrer y mostrar su contenido por pantalla.</li>
+     * </ul>
+     * </p>
+     *
+     * <p>
+     * El programa imprime las temperaturas máximas registradas durante una semana.
+     * </p>
+     *
+     * @author Iván Corrales
+     * @version 12/10/2025
+     */
+    public class IntroduccionArrays {
+
+        /**
+         * Muestra por consola la temperatura máxima correspondiente a cada día de la
+         * semana.
+         * <p>
+         * Ejemplo de salida esperada:
+         * </p>
+         * 
+         * <pre>
+         * Lunes: 22º
+         * Martes: 25º
+         * Miércoles: 24º
+         * ...
+         * Domingo: 18º
+         * </pre>
+         *
+         * @param temperaturas array de enteros que contiene las temperaturas máximas
+         *                     correspondientes a los días de la semana, en orden.
+         *                     Debe tener exactamente 7 elementos.
+         */
+        public void mostrarTemperaturas(int[] temperaturas) {
+            String[] dias = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+
+            for (int i = 0; i < temperaturas.length; i++) {
+                System.out.println(dias[i] + ": " + temperaturas[i] + "º");
+            }
+        }
+
+        /**
+         * Método principal del programa. Crea un array con las temperaturas máximas
+         * de la semana y llama al método {@link #mostrarTemperaturas(int[])} para
+         * mostrar los valores en consola.
+         *
+         * @param args argumentos de línea de comandos (no utilizados en este ejemplo).
+         */
+        public static void main(String[] args) {
+
+            // 1. Declara un array de tipo int llamado maxTemperaturas que tiene 7 elementos
+            // Cada elemento se corresponde con un día de la semana: Lunes, ... Domingo
+
+            // 2. Los valores son: 22, 23, 24, 24 , 23, 21, 20
+
+            // 3. Llama a la función mostrarTemperaturas
+
+        }
+
+    String[] paises = new String[5];paises[0]="Afganistan";paises[1]="España";paises[2]="Perú";paises[3]="Colombia";paises[4]="Venezuela";System.out.printf("Paises [0]: %s\n",paises[0]);System.out.printf("Paises [4]: %s\n",paises[4]);System.out.printf("Total de paises: %d\n",paises.length);
+    }
+}
