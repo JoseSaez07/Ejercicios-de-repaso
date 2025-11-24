ae8506a1161dcd37b4d2c6bd7951cc18c0e045c5
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-24 16:24:37 +0100

    jose
    saez

--- examen.java
+++ examen.java
@@ -1,3 +1,9 @@
-public class examen {
+import java.util.Scanner;
 
+public class examen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==Centro de control==");
+        System.out.println("1.Mos");
+    }
 }
