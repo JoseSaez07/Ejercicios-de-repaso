b8e9c8e113fc7bb9db07478e75bec02742cbb3ca
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-24 11:34:04 +0100

    a

---
+++ jose09.java
@@ -0,0 +1,10 @@
+public class jose09 {
+public static void main(String[] args) {
+   String[] ciudades = new String[]{"Málaga","Madrid","Sevilla"};
+   
+   
+}
+ public static void devolverArray(int[] ) {
+    
+ }
+}
