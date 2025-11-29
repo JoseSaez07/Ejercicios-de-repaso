c8b821098402d7b9ea44e27b1b8cccb84777d123
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-29 15:25:55 +0100

    s
    s

---
+++ nose.java
@@ -0,0 +1,9 @@
+public class nose {
+
+    public static void imprimirRaiz(double[] numeros) {
+        for (int i = 0; i < numeros.length; i++) {
+            double raiz = Math.sqrt(numeros[i]);
+            System.out.println(raiz);
+        }
+    }
+}
